92db1cd49fe09744b00a7d57b7c0270a7877e54c
Erick <erick.r.sampaio@hotmail.com>
2022-05-30 20:18:05 -0300

    RPG-Git

---
+++ RPG/src/Warrior.cs
@@ -0,0 +1,12 @@
+namespace RPG.src
+{
+    public class Warrior : Hero
+    {
+        public Warrior(string name, int level, string heroType)
+        {
+            this.name = name;
+            this.level = level;
+            this.heroType = heroType;
+        }
+    }
+}
